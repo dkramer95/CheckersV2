3f0daa62520f01c13ef209a839dca0118016b65e
Tryston Maris <tmaris@student.neumont.edu>
2016-11-04 09:29:26 -0600

    Got rid of Projects and added folders.

---
+++ Checkers/Checkers/Model/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersModel
+{
+    public abstract class Player
+    {
+        public Color PiecesColor { get; set; }
+
+        public Move GetMove(IEnumerable<Move> moves);
+
+
+    }
+}
---
+++ Checkers/Checkers/Model/Square.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersModel
+{
+    /// <summary>
+    /// This class represents a single individual square. A square can
+    /// hold at most one piece, or it can be empty.
+    /// </summary>
+    public class Square
+    {
+        public Piece Piece { get; private set; }
+
+
+        public bool AddPiece(Piece piece)
+        {
+            if (HasPiece())
+            {
+                // Cannot occupy this Square because we haven't removed
+                // an existing piece already
+            } else
+            {
+                Piece = piece;
+            }
+        }
+
+        public bool HasPiece()
+        {
+            bool result = (Piece != null);
+            return result;
+        }
+
+        public void RemovePiece()
+        {
+            Piece = null;
+        }
+    }
+}
