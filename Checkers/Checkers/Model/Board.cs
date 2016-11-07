2ad82f90aafa0db55613b1ddeaa9ebf1ebb53540
Tryston Maris <tmaris@student.neumont.edu>
2016-11-07 08:00:57 -0700

    IDK.

--- Checkers/Checkers/Model/Board.cs
+++ Checkers/Checkers/Model/Board.cs
@@ -73,7 +73,7 @@ namespace Checkers.Model
             {
                 "B8", "D8", "F8", "H8",
                 "A7", "C7", "E7", "G7",
-                "B6", "D6", "F6", "H6",
+                "B6", "D6", "F6", "H6"
             };
 
             string[] lightPositions = new string[]
