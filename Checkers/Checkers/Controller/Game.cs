f5bb72ddc050a5d063994206a0ad2df656ab96db
David Kramer <dkramer@student.neumont.edu>
2016-11-07 16:35:51 -0700

    Added UndoStack to allow for reverting back to previous BoardStates.

--- Checkers/Checkers/Controller/Game.cs
+++ Checkers/Checkers/Controller/Game.cs
@@ -10,7 +10,20 @@ namespace Checkers.Controller
 {
     class Game
     {
+        private UndoStack undoStack;
+
         List<Player> players;
+
+        public Game()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            undoStack = new UndoStack();
+        }
+
         public void Start()
         {
             TypeMenu types = new TypeMenu();
@@ -19,9 +32,14 @@ namespace Checkers.Controller
             Board.Populate(players[0].Pieces, players[1].Pieces);
             // testing
             Console.WriteLine(Board.ToString());
-
             GamePlay();
         }
+
+        private void ExportState()
+        {
+            undoStack.Push(GameState.ExportFromBoardSquares(Board.GridSquares));
+        }
+
         private void GamePlay()
         {
             PieceController p = new PieceController();           
@@ -39,6 +57,7 @@ namespace Checkers.Controller
                 {
                     pturn = 0;
                 }
+                // TODO:: after every turn export the state of the board
             }
         }
         private void MovePiece(Move m)
@@ -46,6 +65,25 @@ namespace Checkers.Controller
             Piece p=Board.SquareAt(m.StartPosition.Column, m.StartPosition.Column).Piece;
             Board.SquareAt(m.EndPosition.Column, m.EndPosition.Row).AddPiece(p);
             Board.SquareAt(m.StartPosition.Column, m.StartPosition.Column).RemovePiece();
+        }
+
+        private void UndoMove()
+        {
+            // can't undo if we have nothing to fall back to
+            if (undoStack.Size() > 0)
+            {
+                // clear out the board and just have empty squares
+                Board.Reset();
+
+                GameState undoState = undoStack.Pop();
+
+                // for every piece in the state, add place it on the board
+                foreach(Position pos in undoState.PieceLayout.Keys)
+                {
+                    Piece piece = undoState.PieceLayout[pos];
+                    Board.SquareAt(pos).AddPiece(piece);
+                }
+            }
         }
     }
 }
