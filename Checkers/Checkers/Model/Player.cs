5b6e6d6383ed420bc7d805252e0d5f505baa1b6e
Chance Hollis <chancehollis@gmail.com>
2016-11-07 08:29:31 -0700

    Added Pieces property to Player

--- Checkers/Checkers/Model/Player.cs
+++ Checkers/Checkers/Model/Player.cs
@@ -10,6 +10,8 @@ namespace Checkers.Model
     {
         public Color PiecesColor { get; set; }
 
+        public IEnumerable<Piece> Pieces { get; set; }
+
         public abstract Move GetMove(IEnumerable<Move> moves);
 
 
