9344c8a788d76651544195dc981bdef0b08458db
David Kramer <dkramer@student.neumont.edu>
2016-11-08 08:14:17 -0700

    Begin work on creating GUI for Checkers
    * New Project containing GUI created and contained within Solution
    * Basic uniform 8x8 grid created

---
+++ Checkers/CheckersGUI/BoardView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace CheckersGUI
+{
+    public class BoardView : UniformGrid
+    {
+        public List<SquareView> Squares { get; private set; }
+
+        public BoardView()
+        {
+            Columns = 8;
+            Rows = 8;
+            AddSquares();
+        }
+
+        private void AddSquares()
+        {
+            Squares = new List<SquareView>();
+
+            for (int j = 0; j < 64; ++j)
+            {
+                SquareView square = new SquareView();
+                Squares.Add(square);
+                Children.Add(square);
+            }
+        }
+    }
+}
---
+++ Checkers/CheckersGUI/SquareView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CheckersGUI
+{
+    public class SquareView : Label
+    {
+        public SquareView()
+        {
+            //Background = Brushes.Transparent;
+            Background = Brushes.Red;
+            BorderBrush = Brushes.Black;
+            BorderThickness = new Thickness(3);
+        }
+    }
+}
